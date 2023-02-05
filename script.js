b4958967a7ec92d49381dc5fd655d7d07b7e88e2
Tanmay Samanta <121569819+Tamy001@users.noreply.github.com>
2023-02-05 10:36:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < name.length; i++) {
+    var firstLetter = name[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+        helloSpeaker.speak(name[i]);
+      } else {
+        helloSpeaker.speak(name[i]);
+      }
+    }
+    
+    })();
+    
